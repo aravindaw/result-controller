d1d3a0713b82a48b94f37c59d13a1c6a4f4487b8
Aravinda Weerasekara <aravinda.weerasekara@check24.de>
2020-07-29 14:31:16 +0200

    update generator.sh file

--- generator.sh
+++ generator.sh
@@ -6,8 +6,11 @@ TMP_PATH=/home/allure-result
 echo "$RESULT_PATH" generating >> /home/result-controller/info.log
 echo "$RESULT_PATH" >> /home/result-controller/info.log
 echo "$REPORT_PATH" >> /home/result-controller/info.log
+# unzip and remove zipped result file
 [ -f "$RESULT_PATH" ] && tar -xf "$RESULT_PATH" -C /home && rm -rf "$RESULT_PATH"
 echo extract tar file >> /home/result-controller/info.log
+# Remove history file if existing in the result folder
+[ ! -f "$TMP_PATH"/history ] && rm -rf "$TMP_PATH"/history
 [ ! -f "$REPORT_PATH_PATH"/history ] && mv "$REPORT_PATH"/history $TMP_PATH/history
 echo move history file to result folder >> /home/result-controller/info.log
 allure generate "$TMP_PATH" --clean -o "$REPORT_PATH"
