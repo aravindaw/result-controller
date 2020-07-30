a9ea4bf60320e8e5b92e66b4ded4c1d41a3df4fe
Aravinda Weerasekara <aravinda.weerasekara@check24.de>
2020-07-30 10:26:20 +0200

    update generator.sh and docker-compose file

--- generator.sh
+++ generator.sh
@@ -1,17 +1,18 @@
 export RESULT_PATH=$1
 export REPORT_PATH=$2
 TMP_PATH=/home/allure-result
+BACKUP_PATH=/home/backup
 
 # shellcheck disable=SC2129
 echo "$RESULT_PATH" generating >> /home/result-controller/info.log
 echo "$RESULT_PATH" >> /home/result-controller/info.log
 echo "$REPORT_PATH" >> /home/result-controller/info.log
-# unzip and remove zipped result file
-[ -f "$RESULT_PATH" ] && tar -xf "$RESULT_PATH" -C /home && rm -rf "$RESULT_PATH"
+# unzip and backup zipped result file
+[ -f "$RESULT_PATH" ] && tar -xf "$RESULT_PATH" -C /home && mv "$RESULT_PATH" $BACKUP_PATH
 echo extract tar file >> /home/result-controller/info.log
 # Remove history file if existing in the result folder
 [ ! -f "$TMP_PATH"/history ] && rm -rf "$TMP_PATH"/history
-[ ! -f "$REPORT_PATH_PATH"/history ] && mv "$REPORT_PATH"/history $TMP_PATH/history
+[ ! -f "$REPORT_PATH"/history ] && mv "$REPORT_PATH"/history $TMP_PATH/history
 echo move history file to result folder >> /home/result-controller/info.log
 allure generate "$TMP_PATH" --clean -o "$REPORT_PATH"
 echo generate report folder from result folder >> /home/result-controller/info.log
